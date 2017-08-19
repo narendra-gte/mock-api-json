ff3a62e966bb0c296d5633f0c1cdac3adf0562de
Narendra <narendra@globaltestingexperts.com>
2017-08-19 11:39:13 +0530

    add leeway time

--- lib/auth.rb
+++ lib/auth.rb
@@ -2,14 +2,13 @@ require 'jwt'
 
 class Auth
   ALGORITHM = 'HS256'
-  leeway = 10 # seconds
 
   def self.decode(token)
     begin
       JWT.decode(token,
         auth_secret,
         true,
-        { :exp_leeway => leeway, :algorithm => ALGORITHM }).first
+        { :exp_leeway => 10, :algorithm => ALGORITHM }).first
     rescue JWT::ExpiredSignature
       nil
     end
