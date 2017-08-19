ce59fcae05747c9087b03b5d619d7ca382b10fcd
= <=>
2017-08-19 13:24:31 +0530

    status changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  enum status: { active: 1, archived: 0, pending_category_approval:-1 }
+  enum status: { active: 1, active_archived: 0, pending_category_approval:-1,:pending_archived:2 }
   has_many :fine_prints
   has_many :purchase_options
   has_many :images_products
