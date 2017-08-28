97697232968b8378079f00a3b2430c492828243f
nitin <nitin@globaltestingexperts.com>
2017-08-28 12:46:22 +0530

    change in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  enum status: { active: 1, active_archived: 0, pending_category_approval:-1,pending_archived:2 }
+  enum status: { active: 1, active_archived: 0, pending_category_approval:-1,pending_archived:2 } 
   has_many :fine_prints
   has_many :purchase_options
   has_many :images_products
